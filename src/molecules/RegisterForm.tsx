746cdc67e7bb99d4ce570730b04e6c1d65624d3a
esteban2841 <estebanpuentesdev@gmail.com>
2025-03-03 00:39:28 -0500

    Feature: implemented registration flow integrated with nbackend

--- src/molecules/RegisterForm.tsx
+++ src/molecules/RegisterForm.tsx
@@ -2,47 +2,41 @@
 import { FormEvent } from "react"
 import { CustomButton, CustomInput, CustomText } from "@/atoms"
 import { useRouter } from "next/navigation"
+import axios from "axios"
 
 
 export const RegisterForm = () => {
 
+  const uri = 'http://localhost:3000'
+  
   const router = useRouter()
-
+  
   const handleRegister = async (e: FormEvent<HTMLFormElement>)=>{
     e.preventDefault()
+    console.log("TCL: RegisterForm -> uri", uri)
 
     const formData = {
-      nombre: new FormData(e.currentTarget).get('nombre'),
-      correo: new FormData(e.currentTarget).get('correo'),
-      usuario: new FormData(e.currentTarget).get('usuario'),
-      contraseña: new FormData(e.currentTarget).get('contraseña'),
+      name: new FormData(e.currentTarget).get('nombre'),
+      email: new FormData(e.currentTarget).get('correo'),
+      username: new FormData(e.currentTarget).get('usuario'),
+      password: new FormData(e.currentTarget).get('contraseña'),
     }
-
+    
+    const data = JSON.parse(JSON.stringify(formData))
+		console.log("TCL: handleRegister -> data", data)
     try {
-      const registerResponse = await fetch(
-        '/api/register',
+      const registerResponse = await axios.post(
+        `${uri}/api/auth/register`,
         {
-          method: 'POST',
-          headers:{
-            'Content-Type': 'application/json'
-          },
-          cache: 'no-cache',
-          body: JSON.stringify(formData)
+          ...data
         }
       )
+      console.log("TCL: handleRegister -> registerResponse", registerResponse)
       if(registerResponse.status == 200){
-        const loginResponse = await fetch(
-          '/api/login',
+        const loginResponse = await axios(
+          `${uri}/api/auth/login`,
           {
-            method: 'POST',
-            headers:{
-              'Content-Type': 'application/json'
-            },
-            cache: 'no-cache',
-            body: JSON.stringify({
-              correo: formData.correo, 
-              contraseña: formData.contraseña
-            })
+            ...data
           }
         )
         if(loginResponse.status == 200){
@@ -64,7 +58,7 @@ export const RegisterForm = () => {
             <CustomInput name="correo" content="Correo electronico" theme="dark" type="text" withLabel={false}/>
             <CustomInput name="usuario" content="Usuario" theme="dark" type="text" withLabel={false}/>
             <CustomInput name="contraseña" content="Contraseña" theme="dark" type="password" withLabel={false}/>
-            <CustomButton bg="white" colorText="black" bold={true} hoverColor="white" hoverBg="rgb(23 37 84)"  type="submit" content="Registrarme ahora" action="userRegistration"/>
+            <CustomButton bg="white" colorText="black" bold={true} hoverColor="white" hoverBg="rgb(23 37 84)"  type="submit" content="Registrarme ahora"/>
         </div>
     </form>
   )
